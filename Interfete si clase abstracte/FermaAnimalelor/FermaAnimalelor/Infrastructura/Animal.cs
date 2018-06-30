816c664f2dadbc3d0113b4d13c89e80b1413cb15
Romulus Crisan <cromica@gmail.com>
2018-06-30 15:15:18 +0300

    Problema interfete si clase abstracte

---
+++ Interfete si clase abstracte/FermaAnimalelor/FermaAnimalelor/Infrastructura/Animal.cs
@@ -0,0 +1,36 @@
+﻿using FermaAnimalelor.Infrastructura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermaAnimalelor
+{
+    abstract class Animal
+    {
+        public bool AnimalHranit { get; private set; }
+
+        public bool AnimalAdormit { get; private set; }
+        protected abstract string Rasa { get; }
+        protected abstract IHrana HranaPreferata { get; }
+        public void Mananca(IHrana hrana)
+        {
+            if(hrana.Denumire == HranaPreferata.Denumire)
+            {
+                AnimalHranit = true;
+            }
+        }
+
+        public void Doarme()
+        {
+            if (AnimalHranit)
+            {
+                AnimalAdormit = true;
+            }
+           
+        }
+
+        public abstract IResursa ColecteazaResurse();
+    }
+}
---
+++ Interfete si clase abstracte/FermaAnimalelor/FermaAnimalelor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FermaAnimalelor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Intr-o ferma se afla 3 vaci, 2 oi si 5 gaini. Defineste clasele corespunzatoare
+            //folosind clasele si interfetele predefinite fara a le modifica in vreun fel si 
+            // afiseaza raspunsurile la cele 4 intrebari de mai jos.
+
+            //adauga animalele in lista predefinita
+            var animale = new Animal[10];
+
+
+            //1. Tipareste numarul de gaini calculandul programatic
+
+
+            //2. Hraneste animalele cu tipul de hrana adecvat. Tipurile de hrana vor fi definite
+            // folosind IHRana si create in folderul Hrana
+
+
+            //3. Colecteaza toate resursele produse de animale (oua, lapte) si afisati totalul cantitatii
+            //   colectate pentru fiecare din ele.
+            //   Tipurile de resurse vor fi definite folosind IResursa si vor fi puse in folderul
+            //   Resurse
+
+
+            //4. Pune animalele la somn. Pentru a ne asigura ca animalele sunt la somn se va 
+            // verifica ca fiecare Animal este "AnimalAdormit"- proprietate disponibila pe clasa 
+            // animal. 
+
+            Console.ReadLine();
+        }
+    }
+}
